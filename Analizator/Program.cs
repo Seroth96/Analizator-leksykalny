72f4a5f021c925ee63f68ae51647c21d2cf0f47c
Paweł <pbokotko@gmail.com>
2019-03-31 12:53:19 +0200

    Dodaj pliki projektów.

---
+++ Analizator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Analizator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
